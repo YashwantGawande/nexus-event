6617289f16d116927a57d4813659ffb927c45057
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-05 23:47:07 +0530

    Create sidebarItem

---
+++ src/components/layout/SidebarItem.tsx
@@ -0,0 +1,13 @@
+import { IconType } from "react-icons";
+
+interface SidebarItemProps {
+  label: string;
+  href: string;
+  icon: IconType;
+  onClick?: () => void;
+}
+const SidebarItem = () => {
+  return <div></div>;
+};
+
+export default SidebarItem;

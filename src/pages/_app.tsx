f60d1f36ab37ff3fb5eaf9490a8dcfa2cc2a92b1
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-07 23:56:17 +0530

    Cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<>
-  <Modal isOpen title ="Test Modal"/>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout></>
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }

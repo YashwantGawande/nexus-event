93ffca39b8270248f52af5625cfd20fd0f4f250c
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-07 17:18:42 +0530

    Import modal in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
+  return (<>
+  <Modal isOpen title ="Test Modal"/>
     <Layout>
       <Component {...pageProps} />
-    </Layout>
+    </Layout></>
   );
 }

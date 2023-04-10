9d2e384988f46270195ce8c8f9347669de851870
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-10 23:55:33 +0530

    Update sidebar

--- src/components/layout/SidebarPostButton.tsx
+++ src/components/layout/SidebarPostButton.tsx
@@ -1,20 +1,21 @@
 import { useRouter } from "next/router";
 import { BsFillSignpostFill } from "react-icons/bs";
+import useLoginModal from "../hooks/useLoginModal";
+import { useCallback } from "react";
 
 const SidebarPostButton = () => {
   const router = useRouter();
+  // const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    // loginModal.onOpen();
+  }, [/*loginModal*/]);
   return (
-    <div onClick={() => router.push("/")}>
-      <div
-        className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center
-    justify-center bg-purple-500 hover:bg-opacity-80 transition cursor-pointer"
-      >
+    <div onClick={onClick}>
+      <div className="flex items-center justify-center p-4 mt-6 transition bg-purple-500 rounded-full cursor-pointer lg:hidden h-14 w-14 hover:bg-opacity-80">
         <BsFillSignpostFill size={24} color="white" />
       </div>
-      <div
-        className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-purple-500 hover:bg-opacity-90
-      cursor-pointer transition"
-      >
+      <div className="hidden px-4 py-2 mt-6 transition bg-purple-500 rounded-full cursor-pointer lg:block hover:bg-opacity-90">
         <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
           Create
         </p>

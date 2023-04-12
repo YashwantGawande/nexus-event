2d84c549aa48390c2a74c66caa15254fd3ea83f2
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-13 00:31:09 +0530

    Add useCurrentUser hook for fetching current user data using SWR

---
+++ src/components/hooks/useCurrentUser.ts
@@ -0,0 +1,8 @@
+import useSwr from "swr";
+import fetcher from "@/libs/fetcher";
+const useCurrentUser = () => {
+  const { data, error, isLoading, mutate } = useSwr("/api/current", fetcher);
+  return { data, error, isLoading, mutate };
+};
+
+export default useCurrentUser;

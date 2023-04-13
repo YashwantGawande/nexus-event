528f69450db68e7d1a47853deea62919cb0530f1
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-13 21:44:47 +0530

    Added bcrypt password hashing and user JSON response

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,6 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+
 import prisma from "@/libs/prismadb";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -8,6 +10,7 @@ export default async function handler(
   if (req.method !== "POST") {
     return res.status(405).end();
   }
+
   try {
     const { email, username, name, password } = req.body;
 
@@ -22,7 +25,7 @@ export default async function handler(
       },
     });
 
-    return res.status(200).json(user)
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.status(400).end();

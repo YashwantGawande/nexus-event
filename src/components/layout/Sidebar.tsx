597d3d2aee645bd5bdea1a5b522feba1c70f0dd5
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-14 23:03:27 +0530

    Enhance Sidebar with authentication, user data, and logout handling

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,10 +2,13 @@ import { BsHouse, BsBellFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
 import SidebarLogo from "./SidebarLogo";
-import SidebarItem from "./SidebarItem";
 import SidebarPostButton from "./SidebarPostButton";
+import useCurrentUser from "../hooks/useCurrentUser";
+import { signOut } from "next-auth/react";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
+  const { data: currentUser } = useCurrentUser();
   const items = [
     {
       label: "Home",
@@ -16,15 +19,17 @@ const Sidebar = () => {
       label: "Notifications",
       href: "/notifications",
       icon: BsBellFill,
+      auth: true,
     },
     {
       label: "Profile",
       href: "/users/123",
       icon: FaUser,
+      auth: true,
     },
   ];
   return (
-    <div className="col-span-1 h-full pr-4 md:pr-6">
+    <div className="h-full col-span-1 pr-4 md:pr-6">
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
@@ -34,9 +39,16 @@ const Sidebar = () => {
               href={item.href}
               label={item.label}
               icon={item.icon}
+              auth={item.auth}
             />
           ))}
-          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+          {currentUser && (
+            <SidebarItem
+              onClick={() => signOut()}
+              icon={BiLogOut}
+              label="Logout"
+            />
+          )}
           <SidebarPostButton />
         </div>
       </div>

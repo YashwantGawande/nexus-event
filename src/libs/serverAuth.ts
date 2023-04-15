9dac44de4b57bd69505a31c3798a9c2185046edb
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-15 23:19:29 +0530

    Update serverAuth

--- src/libs/serverAuth.ts
+++ src/libs/serverAuth.ts
@@ -1,11 +1,10 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest } from "next";
 
 import prisma from "@/libs/prismadb";
-import { authOptions } from "@/pages/api/auth/[nextauth]";
-import { getServerSession } from "next-auth";
+import { getSession } from "next-auth/react";
 
-const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getServerSession(req, res, authOptions);
+const serverAuth = async (req: NextApiRequest) => {
+  const session = await getSession({ req });
 
   if (!session?.user?.email) {
     throw new Error("Not Signed in");

20d569c7bb635378b0eebdef3dc75bb6224f713b
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-18 23:25:56 +0530

    Add Form and PostFeed components to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
+import Form from "@/components/users/Form";
 
 export default function Home() {
   return (
     <>
       <Header label="Home" />
+      <Form placeholder="What's happening"/>
+      <PostFeed />
     </>
   );
 }

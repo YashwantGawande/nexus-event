47a63bf3230afe58d1debf023632a575be7a1fcf
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-19 23:52:02 +0530

    Add onLike function and createdAt to Tweet component

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -1,7 +1,12 @@
 import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+import { formatDistanceToNowStrict } from "date-fns";
+
 import useLoginModal from "../hooks/useLoginModal";
 import useCurrentUser from "../hooks/useCurrentUser";
-import { useCallback } from "react";
+import LoginModal from "../modals/LoginModal";
+import Avatar from "../Avatar";
+import { AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
 
 interface PostItemProps {
   data: Record<string, any>;
@@ -26,7 +31,60 @@ const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
     router.push(`/posts/${data.id}`);
   }, [router, data.id]);
 
-  return <div>PostItem</div>;
+  const onLike = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      loginModal.onOpen();
+    },
+    [LoginModal]
+  );
+
+  const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+      return null;
+    }
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data?.createdAt]);
+
+  return (
+    <div
+      onClick={goToPost}
+      className="border-b-[1px] border-neutral-800 p-5 cursor-Pointer hover:bg-neutral-900 transition"
+    >
+      <div className="flex flex-row items-start gap-3">
+        <Avatar userId={data.user.id} />
+        <div>
+          <div className="flex flex-row items-center gap-2">
+            <p
+              className="font-semibold text-white cursor-pointer hover:underline"
+              onClick={goToUser}
+            >
+              {data.user.name}
+            </p>
+            <span
+              className="hidden cursor-pointer text-neutral-500 hover:underline md:block"
+              onClick={goToUser}
+            >
+              @{data.user.username}
+            </span>
+            <span className="text-sm text-neutral-500">{createdAt}</span>
+          </div>
+          <div className="mt-1 text-white">{data.body}</div>
+          <div className="flex flex-row items-center gap-10 mt-3">
+            <div className="flex flex-row items-center gap-2 transition cursor-pointer text-neutral-500 hover:text-purple-500">
+              <AiOutlineMessage size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+            <div className="flex flex-row items-center gap-2 transition cursor-pointer text-neutral-500 hover:text-pink-500" onClick={onLike}>
+              <AiOutlineHeart size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostItem;

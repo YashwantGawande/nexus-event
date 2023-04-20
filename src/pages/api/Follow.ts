54d583a2e1b2261f45f38a02f4e62f66b1c0305e
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-20 23:47:57 +0530

    Refractor the code

--- src/pages/api/Follow.ts
+++ src/pages/api/Follow.ts
@@ -10,16 +10,17 @@ export default async function handler(
   if (req.method !== "POST" && req.method !== "DELETE") {
     return res.status(405).end();
   }
+
   try {
     const { userId } = req.body;
-    
+
     const { currentUser } = await serverAuth(req, res);
 
     if (!userId || typeof userId !== "string") {
       throw new Error("Invalid ID");
     }
 
-    const user = await prisma?.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         id: userId,
       },
@@ -34,6 +35,7 @@ export default async function handler(
     if (req.method === "POST") {
       updatedFollowingIds.push(userId);
     }
+
     if (req.method === "DELETE") {
       updatedFollowingIds = updatedFollowingIds.filter(
         (followingId) => followingId !== userId
@@ -48,8 +50,8 @@ export default async function handler(
         followingIds: updatedFollowingIds,
       },
     });
-    return res.status(200).json(updatedUser)
 
+    return res.status(200).json(updatedUser);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
